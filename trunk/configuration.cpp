71ec25f9ae74bda6c5317f6c7a5b31996a24391f
vojta.jina <vojta.jina@f71fc35e-9e61-cdbf-5a40-9e5a3efe5e49>
2010-04-02 11:23:01 +0000

    Configuration - whole implementation main - parsing arguments and displaying available positions

--- trunk/configuration.cpp
+++ trunk/configuration.cpp
@@ -1,13 +1,38 @@
 #include "configuration.h"
 
-Configuration::Configuration()
+Configuration::Configuration(int k, int q, int* F, int K)
 {
-  this->isFinal = false;
-  this->figuresPosition = new int[2];
-  this->figuresCount = 0;
-  this->queenSteps = new int[2];
-  this->queenStepsCount = 0;
-  this->queenPosition = 0;
+  fieldLength = k * k;
+  sideLength = k;
+  figuresPosition = new bool[fieldLength];
+  figuresStartPosition = new bool[fieldLength];
+  figuresCount = q;
+  queenSteps = new int[fieldLength];
+  queenStepsCount = 0;
+
+  // add queen start position to queenSteps history
+  queenSteps[0] = K;
+
+  // load figure positions
+  int a = 0;
+  for (int i = 0; i < fieldLength; i++)
+  {
+    // there is a figure on i position
+    if (a < q && F[a] ==i)
+    {
+      figuresPosition[i] = true;
+      figuresStartPosition[i] = true;
+    }
+    // no figure on i position
+    else
+    {
+      figuresPosition[i] = false;
+      figuresStartPosition[i] = false;
+    }
+
+    if (F[a]==i)
+      a++;
+  }
 }
 
 IntPriQueue* Configuration::getAvailablePositions() const
@@ -16,23 +41,204 @@ IntPriQueue* Configuration::getAvailablePositions() const
   // priority 0 = position with figure
   // priority 1 = free position
   IntPriQueue* pq = new PriorityQueue<int>(2);
-  pq->push(1, 0);
 
+  this->addAvailableTop(pq);
+  this->addAvailableBottom(pq);
+  this->addAvailableLeft(pq);
+  this->addAvailableRight(pq);
+  this->addAvailableLeftTop(pq);
+  this->addAvailableRightTop(pq);
+  this->addAvailableLeftBottom(pq);
+  this->addAvailableRightBottom(pq);
+  
   return pq;
 }
 
-bool Configuration::getIsFinal() const
+bool Configuration::final() const
 {
+  return (figuresCount == 0);
 }
 
 int Configuration::getStepsCount() const
 {
+  return queenStepsCount;
 }
 
 void Configuration::move(int newPosition)
 {
+  queenStepsCount++;
+  queenSteps[queenStepsCount] = newPosition;
+  this->removeFigure(newPosition);
 }
 
 void Configuration::moveBack()
 {
+  if (queenStepsCount > 0)
+  {
+    this->restoreFigure(queenSteps[queenStepsCount]);
+    //queenSteps[queenStepsCount] = -1;
+    queenStepsCount--;
+  }
+}
+
+// PRIVATE METHODS
+bool Configuration::removeFigure(int position)
+{
+  // no figure on the position
+  if (!figuresPosition[position])
+    return false;
+
+  figuresPosition[position] = false;
+  figuresCount--;
+  return true;
+}
+
+bool Configuration::restoreFigure(int position)
+{
+  // the figure is already on this position
+  // or there was no figure on the start
+  if (figuresPosition[position] || !figuresStartPosition[position])
+    return false;
+
+  figuresPosition[position] = true;
+  figuresCount++;
+  return true;
+}
+
+int Configuration::getQueenPosition() const
+{
+  return queenSteps[queenStepsCount];
+}
+
+void Configuration::addAvailableLeft(IntPriQueue* pq) const
+{
+  // init
+  int queenPos = this->getQueenPosition();
+  int y = queenPos - 1;
+  int min = queenPos - (queenPos % sideLength);
+  int priority = 1;
+
+  while (y >= min && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y - 1;
+  }
+}
+
+void Configuration::addAvailableRight(IntPriQueue* pq) const
+{
+  // init
+  int queenPos = this->getQueenPosition();
+  int y = queenPos + 1;
+  int max = queenPos - (queenPos % sideLength) + sideLength;
+  int priority = 1;
+
+  while (y < max && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y + 1;
+  }
+
+}
+
+void Configuration::addAvailableTop(IntPriQueue* pq) const
+{
+  // init
+  int y = this->getQueenPosition() - sideLength;
+  int priority = 1;
+
+  while (y >= 0 && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y - sideLength;
+  }
+}
+
+void Configuration::addAvailableBottom(IntPriQueue* pq) const
+{
+  // init first position
+  int y = this->getQueenPosition() + sideLength;
+  int priority = 1;
+
+  while (y < fieldLength && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y + sideLength;
+  }
+}
+
+void Configuration::addAvailableLeftTop(IntPriQueue* pq) const
+{
+  // init first position
+  int y = this->getQueenPosition() - sideLength - 1;
+  int priority = 1;
+
+  while (y >= 0 && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y - sideLength - 1;
+  }
+}
+
+void Configuration::addAvailableRightTop(IntPriQueue* pq) const
+{
+  // init first position
+  int y = this->getQueenPosition() - sideLength + 1;
+  int priority = 1;
+
+  while (y > 0 && (y % sideLength) > 0 && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y - sideLength + 1;
+  }
+}
+
+void Configuration::addAvailableLeftBottom(IntPriQueue* pq) const
+{
+  // init first position
+  int y = this->getQueenPosition() + sideLength - 1;
+  int priority = 1;
+
+  while (y < fieldLength && ((y + 1) % sideLength) > 0 && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y + sideLength - 1;
+  }
+}
+
+void Configuration::addAvailableRightBottom(IntPriQueue* pq) const
+{
+  // init first position
+  int y = this->getQueenPosition() + sideLength + 1;
+  int priority = 1;
+
+  while (y < fieldLength && priority == 1)
+  {
+    priority = this->addAvailablePosition(pq, y);
+    y = y + sideLength + 1;
+  }
+}
+
+int Configuration::addAvailablePosition(IntPriQueue* pq, int position) const
+{
+  // allow to visit each position only once
+  if (this->wasPositionVisited(position))
+    return -1;
+
+  // determine the priority
+  // if there is a figure - priority 0 = high
+  // else - there is no figure - priority 1 = low
+  int priority = (figuresPosition[position]) ? 0 : 1;
+
+  pq->push(position, priority);
+  return priority;
+}
+
+bool Configuration::wasPositionVisited(int position) const
+{
+  for (int i = 0; i<=queenStepsCount; i++)
+    if (queenSteps[i] == position)
+      return true;
+
+  return false;
 }
--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
+#include <stdlib.h>
 #include "configuration.h"
 #include "priority_queue.h"
 
 using namespace std;
 
+void printPQ(IntPriQueue* pq)
+{
+  int a;
+  while (!pq->empty())
+  {
+    a = pq->pop();
+    cout << a << ", ";
+  }
+
+  cout << "\n";
+}
+
+/**
+ * ARGUMENTS
+ * 1 - k = field side length (field length = side x side)
+ * 2 - K = queen position
+ * 3 - q = number figures
+ * 4 - F[q] = array of figure positions
+ */
 int main(int argc, char **argv)
 {
+  // parse inputs
+  int k, K, q;
+  int* F;
+
   try
   {
-    Configuration* cnf;
-    cnf = new Configuration();
+    k = atoi(argv[1]);
+    K = atoi(argv[2]);
+    q = atoi(argv[3]);
 
+    F = new int[q];
+    for (int i = 0; i < q; i++)
+      F[i] = atoi(argv[i+4]);
+
+  }
+  catch (exception& e)
+  {
+    cout << "Bad input arguments (Exception: " << e.what() << ")";
+  }
+
+  // solve
+  try
+  {
+    Configuration* cnf = new Configuration(k, q, F, K);
     IntPriQueue* pq = cnf->getAvailablePositions();
-
-    int a;
-    while (!pq->empty())
-    {
-      a = pq->pop();
-      cout << a << "\n";
-    }
-
+    printPQ(pq);
   }
   catch (exception& e)
   {
