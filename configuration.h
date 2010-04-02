026ce6010afb95b2e981c592743ce2040fc48908
vojta.jina <vojta.jina@f71fc35e-9e61-cdbf-5a40-9e5a3efe5e49>
2010-04-02 11:23:01 +0000

    Configuration - whole implementation main - parsing arguments and displaying available positions

--- configuration.h
+++ configuration.h
@@ -14,16 +14,141 @@
 class Configuration
 {
   private:
-    bool isFinal;
-    int* figuresPosition;
+
+    /**
+     * @brief Current position of remaining figures
+     * Array of bools - true if position is full, false if position is empty
+     */
+    bool* figuresPosition;
+
+    /**
+     * @brief Original position of all figures
+     * Array of bools - true if position is full, false if position is empty
+     * This array is constant - these values wil not change (its for moving back)
+     */
+    bool* figuresStartPosition;
+
+    /**
+     * @brief Number of remaining figures
+     */
     int figuresCount;
+
+    /**
+     * @brief History of queen steps (positions visited)
+     */
     int* queenSteps;
-    int queenPosition;
+
+    /**
+     * @brief Number of steps (visited positions)
+     */
     int queenStepsCount;
-    //void addAvailableRight(PriorityQueue<int>)
+    
+    /**
+     * @brief Size of the field = k x k
+     */
+    int fieldLength;
+    
+    /**
+     * @brief Size of one side of the field = k
+     * fieldLength = sideLength x sideLength
+     */
+    int sideLength;
+
+    /**
+     * @brief Add available positions in left direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableLeft(IntPriQueue* pq) const;
+
+    /**
+     * @brief Add available positions in right direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableRight(IntPriQueue* pq) const;    
+    
+    /**
+     * @brief Add available positions in top direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableTop(IntPriQueue* pq) const;
+    
+    /**
+     * @brief Add available positions in bottom direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableBottom(IntPriQueue* pq) const;
+
+    /**
+     * @brief Add available positions in left-top (diagonal) direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableLeftTop(IntPriQueue* pq) const;
+    
+    /**
+     * @brief Add available positions in right-top (diagonal) direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableRightTop(IntPriQueue* pq) const;
+
+    /**
+     * @brief Add available positions in left-bottom (diagonal) direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableLeftBottom(IntPriQueue* pq) const;
+    
+    /**
+     * @brief Add available positions in right-bottom (diagonal) direction from the queen position
+     * @param pq PriorityQueue
+     */
+    void addAvailableRightBottom(IntPriQueue* pq) const;
+    
+    /**
+     * @brief Abstract method for adding available position into PriorityQueue
+     * This method is used by all methods like addAvailableLeftBottom, addAvailableRightBottom, etc...
+     * Determines the priority of specific position
+     * @param pq PriorityQueu
+     * @param position Index of the position to add
+     * @return Priority of the avaible position
+     */
+    int addAvailablePosition(IntPriQueue* pq, int position) const;
+    
+    /**
+     * @brief Remove figure on specific position
+     * @param position Index of the position
+     * @return True if figure was removed, False if there was no figure on the position
+     */
+    bool removeFigure(int position);
+    
+    /**
+     * @brief Revert removeFigure() - take the figure back
+     * @param position Index of the position
+     * @return True if figure was restored, False if there was no figure on start
+     */
+    bool restoreFigure(int position);
+    
+    /**
+     * @brief Returns index of current position of the queen
+     * @return Index of the position
+     */
+    int getQueenPosition() const;
+    
+    /**
+     * @brief Check whether the position has been visited or not
+     * @param position Index of the position
+     * @return True if position has been visited, False otherwise
+     */
+    bool wasPositionVisited(int position) const;
 
   public:
-    Configuration();
+
+    /**
+     * @brief Constructor
+     * @param k Size of the chess box (k x k)
+     * @param q Number of figures
+     * @param F Array of integers (length q) with positions of the figures
+     * @param K Position of the queen (starting position)
+     */
+    Configuration(int k, int q, int* F, int K);
 
     /**
      * @brief Move queen to new position
@@ -40,7 +165,7 @@ class Configuration
      * @brief Is this configuration final = sollution ?
      * @return True if configuration is final, False otherwise
      */
-    bool getIsFinal() const;
+    bool final() const;
 
     /**
      * @brief Return number of steps done in this configuration
