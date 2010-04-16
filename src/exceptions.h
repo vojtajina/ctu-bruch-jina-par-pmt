689c5aaee7a0c85853e12c8af1e80d47ebad1fa3
vojta.jina <vojta.jina@f71fc35e-9e61-cdbf-5a40-9e5a3efe5e49>
2010-04-16 17:05:43 +0000

    Add new exception type: InvalidPriorityException

--- src/exceptions.h
+++ src/exceptions.h
@@ -1,3 +1,6 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
 #include <exception>
 using namespace std;
 
@@ -16,8 +19,24 @@ using namespace std;
  */
 class InvalidPositionException: public exception
 {
-  virtual const char* what() const throw()
-  {
+  virtual const char* what() const throw() {
     return "This position is invalid !";
   }
-} invalid_position_ex;
+};
+
+
+/**
+ * @class InvalidPriorityException
+ * @author Vojta Jina
+ * @date 04/16/2010
+ * @file exceptions.h
+ * @brief This exception is thrown when someone uses out of range priority
+ */
+class InvalidPriorityException: public exception
+{
+  virtual const char* what() const throw() {
+    return "This priority is invalid !";
+  }
+};
+
+#endif // EXCEPTIONS_H
